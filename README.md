045595250fd28008296ce2e7c1c0e7c4e3a4e1d9
Sebastian <sebastian.legnazzi@est.fi.uncoma.edu.ar>
2021-11-08 17:42:20 -0300

    Agrego Carpetas para el TP

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Trabajo-Practico-FINAL-Int.-a-la-Programacion
+# Trabajo-Practico-FINAL-Int.-a-la-Programacion-
+Trabajo practico final de la materia Introducción 1er Año, 1er Cuatrimestre de la carrera Tecnicatura Universitaria en Desarrollo WEB
---
+++ programaLegnazziStefanoTizzano.php
@@ -0,0 +1,67 @@
+<?php
+include_once("tateti.php");
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/* LEGNAZZI. SEBASTIAN. FAI-3152. TUDW. sebastian.legnazzi@est.fi.uncoma.edu.ar. SebastianLegnazzi */
+/* STEFANO. FRANCO. FAI-2750. TUDW. franco.stefano@est.fi.uncoma.edu.ar. francoDanteStefano */
+/* TIZZANO. AGOSTINA. FAI-3579. TUDW. agostina.tizzano@est.fi.uncoma.edu.ar. agostita */
+
+
+
+
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+
+
+
+
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+
+//Declaración de variables:
+
+
+//Inicialización de variables:
+
+
+//Proceso:
+
+$juego = jugar();
+//print_r($juego);
+//imprimirResultado($juego);
+
+
+
+/*
+do {
+    $opcion = ...;
+
+    
+    switch ($opcion) {
+        case 1: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+
+            break;
+        case 2: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+
+            break;
+        case 3: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+
+            break;
+        
+            //...
+    }
+} while ($opcion != X);
+*/
